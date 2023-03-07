d5e4a1ee376a848673ee7541c1add56112061246
Maksim Omelchenko <omelchenk0.m89@yandex.ru>
2023-03-07 22:11:47 +0700

    Mesto: портирование на «Реакт»

--- src/components/Footer.js
+++
@@ -1,8 +0,0 @@
-function Footer(){
-  return(
-    <footer className="footer">
-      <h3 className="footer__signature">&copy; 2022 Максим Омельченко</h3>
-    </footer>
-  )
-}
-export default Footer
